253e68ea8a19c9fe146060bdea99713d464f14ab
Poke <Poke097.1045@gmail.com>
2023-12-29 11:14:31 -0600

    Added comments for better readability.

--- src/main/java/com/github/clevernucleus/playerex/mixin/client/AbstractInventoryScreenMixin.java
+++ src/main/java/com/github/clevernucleus/playerex/mixin/client/AbstractInventoryScreenMixin.java
@@ -25,38 +25,48 @@ import net.minecraft.util.Identifier;
 
 @Mixin(AbstractInventoryScreen.class)
 abstract class AbstractInventoryScreenMixin<T extends ScreenHandler> extends HandledScreen<T> implements ExScreenData {
-	
-	@Unique
-	private List<Page> playerex_pages = new ArrayList<Page>();
-	
-	private AbstractInventoryScreenMixin(T handler, PlayerInventory inventory, Text title) {
-		super(handler, inventory, title);
-	}
-	
-	private boolean filter(Map.Entry<Identifier, Supplier<Page>> entry) {
-		Identifier key = entry.getKey();
-		return !(key.equals(PlayerExClient.ATTRIBUTES_PAGE) || key.equals(PlayerExClient.COMBAT_PAGE));
-	}
-	
-	@Inject(method = "<init>", at = @At("TAIL"))
-	private void playerex_init(T screenHandler, PlayerInventory playerInventory, Text text, CallbackInfo info) {
-		this.playerex_pages.add(0, PageRegistryImpl.findPage(PlayerExClient.ATTRIBUTES_PAGE));
-		this.playerex_pages.add(1, PageRegistryImpl.findPage(PlayerExClient.COMBAT_PAGE));
-		PageRegistryImpl.pages().entrySet().stream().filter(this::filter).map(Map.Entry::getValue).forEach(page -> this.playerex_pages.add(page.get()));
-	}
-	
-	@Override
-	public int getX() {
-		return this.x;
-	}
-	
-	@Override
-	public int getY() {
-		return this.y;
-	}
-	
-	@Override
-	public List<Page> pages() {
-		return this.playerex_pages;
-	}
+
+    @Unique
+    private List<Page> playerex_pages = new ArrayList<Page>();
+
+    // Constructor for the mixin, called by the superclass constructor
+    private AbstractInventoryScreenMixin(T handler, PlayerInventory inventory, Text title) {
+        super(handler, inventory, title);
+    }
+
+    // Method to filter out specific pages based on their identifiers
+    private boolean filter(Map.Entry<Identifier, Supplier<Page>> entry) {
+        Identifier key = entry.getKey();
+        // Exclude ATTRIBUTES_PAGE and COMBAT_PAGE
+        return !(key.equals(PlayerExClient.ATTRIBUTES_PAGE) || key.equals(PlayerExClient.COMBAT_PAGE));
+    }
+
+    // Inject code at the end of the superclass constructor
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void playerex_init(T screenHandler, PlayerInventory playerInventory, Text text, CallbackInfo info) {
+        // Add ATTRIBUTES_PAGE and COMBAT_PAGE at specific indices
+        this.playerex_pages.add(0, PageRegistryImpl.findPage(PlayerExClient.ATTRIBUTES_PAGE));
+        this.playerex_pages.add(1, PageRegistryImpl.findPage(PlayerExClient.COMBAT_PAGE));
+        
+        // Add other pages (excluding ATTRIBUTES_PAGE and COMBAT_PAGE) from the PageRegistry
+        PageRegistryImpl.pages().entrySet().stream().filter(this::filter).map(Map.Entry::getValue).forEach(page -> this.playerex_pages.add(page.get()));
+    }
+
+    // Implementation of the ExScreenData interface method
+    @Override
+    public int getX() {
+        return this.x;
+    }
+
+    // Implementation of the ExScreenData interface method
+    @Override
+    public int getY() {
+        return this.y;
+    }
+
+    // Implementation of the ExScreenData interface method
+    @Override
+    public List<Page> pages() {
+        return this.playerex_pages;
+    }
 }
--- src/main/java/com/github/clevernucleus/playerex/mixin/client/InventoryScreenMixin.java
+++ src/main/java/com/github/clevernucleus/playerex/mixin/client/InventoryScreenMixin.java
@@ -18,16 +18,22 @@ import net.minecraft.text.Text;
 
 @Mixin(InventoryScreen.class)
 abstract class InventoryScreenMixin extends AbstractInventoryScreen<PlayerScreenHandler> {
-	private InventoryScreenMixin(PlayerEntity player, Text text) {
-		super(player.playerScreenHandler, player.getInventory(), text);
-	}
-	
-	private void playerex_forEachTab(Consumer<TabButtonWidget> consumer) {
-		this.children().stream().filter(e -> e instanceof TabButtonWidget).forEach(e -> consumer.accept((TabButtonWidget)e));
-	}
-	
-	@Inject(method = "render", at = @At("TAIL"))
-	private void playerex_render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo info) {
-		this.playerex_forEachTab(tab -> tab.renderTooltip(matrices, mouseX, mouseY));
-	}
+
+    // Constructor for the mixin, called by the superclass constructor
+    private InventoryScreenMixin(PlayerEntity player, Text text) {
+        super(player.playerScreenHandler, player.getInventory(), text);
+    }
+
+    // Custom method to iterate over TabButtonWidget elements and apply a consumer
+    private void playerex_forEachTab(Consumer<TabButtonWidget> consumer) {
+        // Filter children to get only TabButtonWidget instances and apply the consumer
+        this.children().stream().filter(e -> e instanceof TabButtonWidget).forEach(e -> consumer.accept((TabButtonWidget) e));
+    }
+
+    // Inject code at the end of the render method
+    @Inject(method = "render", at = @At("TAIL"))
+    private void playerex_render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo info) {
+        // Render tooltips for each TabButtonWidget using the playerex_forEachTab method
+        this.playerex_forEachTab(tab -> tab.renderTooltip(matrices, mouseX, mouseY));
+    }
 }
