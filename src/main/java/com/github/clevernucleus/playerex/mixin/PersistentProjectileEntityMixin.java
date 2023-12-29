9e858e7525adbaf88d8016b6d12025a58684f4b9
Poke <Poke097.1045@gmail.com>
2023-12-29 10:46:23 -0600

    Added comments for better readability.

--- src/main/java/com/github/clevernucleus/playerex/mixin/PersistentProjectileEntityMixin.java
+++ src/main/java/com/github/clevernucleus/playerex/mixin/PersistentProjectileEntityMixin.java
@@ -19,44 +19,59 @@ import net.minecraft.world.World;
 
 @Mixin(PersistentProjectileEntity.class)
 abstract class PersistentProjectileEntityMixin extends ProjectileEntity {
-	private PersistentProjectileEntityMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
-		super(entityType, world);
-	}
+    
+    // Constructor for the mixin class
+    private PersistentProjectileEntityMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
-	@Inject(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;length()D"))
-	private void playerex_onEntityHit(EntityHitResult entityHitResult, CallbackInfo info) {
-		PersistentProjectileEntity persistentProjectileEntity = (PersistentProjectileEntity)(Object)this;
-		Entity owner = persistentProjectileEntity.getOwner();
-		
-		if(owner instanceof LivingEntity) {
-			DataAttributesAPI.ifPresent((LivingEntity)owner, ExAPI.RANGED_CRIT_CHANCE, 0, value -> {
-				persistentProjectileEntity.setCritical(false);
-				return 0;
-			});
-		}
-	}
-	
-	@ModifyArg(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"), index = -1)
-	private float playerex_onEntityHit(float i) {
-		PersistentProjectileEntity persistentProjectileEntity = (PersistentProjectileEntity)(Object)this;
-		Entity owner = persistentProjectileEntity.getOwner();
-		float damage = i;
-		
-		if(owner instanceof LivingEntity) {
-			LivingEntity livingEntity = (LivingEntity)owner;
-			final float amount = damage;
-			
-			if(DataAttributesAPI.ifPresent(livingEntity, ExAPI.RANGED_CRIT_CHANCE, persistentProjectileEntity.isCritical(), value -> {
-				boolean cache = livingEntity.getRandom().nextFloat() < value;
-				persistentProjectileEntity.setCritical(cache);
-				return cache;
-			})) {
-				final long l = this.random.nextInt(Math.round(i) / 2 + 2);
-				final float fallback = Math.min(l + (long)i, Integer.MAX_VALUE);
-				damage = DataAttributesAPI.ifPresent(livingEntity, ExAPI.RANGED_CRIT_DAMAGE, fallback, value -> (float)(amount * (1.0 + (10.0 * value))));
-			}
-		}
-		System.out.println("DAMAGE: " + damage);
-		return damage;
-	}
+    // Injecting code into the onEntityHit method at a specific point
+    @Inject(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;length()D"))
+    private void playerex_onEntityHit(EntityHitResult entityHitResult, CallbackInfo info) {
+        // Cast this to PersistentProjectileEntity
+        PersistentProjectileEntity persistentProjectileEntity = (PersistentProjectileEntity)(Object)this;
+        
+        // Get the owner of the projectile
+        Entity owner = persistentProjectileEntity.getOwner();
+        
+        // Check if the owner is a living entity
+        if(owner instanceof LivingEntity) {
+            // Modify the critical hit chance using DataAttributesAPI
+            DataAttributesAPI.ifPresent((LivingEntity)owner, ExAPI.RANGED_CRIT_CHANCE, 0, value -> {
+                persistentProjectileEntity.setCritical(false);
+                return 0;
+            });
+        }
+    }
+    
+    // Modifying an argument in the onEntityHit method
+    @ModifyArg(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"), index = -1)
+    private float playerex_onEntityHit(float i) {
+        // Cast this to PersistentProjectileEntity
+        PersistentProjectileEntity persistentProjectileEntity = (PersistentProjectileEntity)(Object)this;
+        // Get the owner of the projectile
+        Entity owner = persistentProjectileEntity.getOwner();
+        float damage = i;
+        
+        // Check if the owner is a living entity
+        if(owner instanceof LivingEntity) {
+            LivingEntity livingEntity = (LivingEntity)owner;
+            final float amount = damage;
+            
+            // Modify the critical hit chance and damage using DataAttributesAPI
+            if(DataAttributesAPI.ifPresent(livingEntity, ExAPI.RANGED_CRIT_CHANCE, persistentProjectileEntity.isCritical(), value -> {
+                boolean cache = livingEntity.getRandom().nextFloat() < value;
+                persistentProjectileEntity.setCritical(cache);
+                return cache;
+            })) {
+                final long randomOffset = this.random.nextInt(Math.round(i) / 2 + 2);
+                final float fallback = Math.min(randomOffset + (long)i, Integer.MAX_VALUE);
+                damage = DataAttributesAPI.ifPresent(livingEntity, ExAPI.RANGED_CRIT_DAMAGE, fallback, value -> (float)(amount * (1.0 + (10.0 * value))));
+            }
+        }
+        
+        // Print the modified damage for debugging purposes
+        System.out.println("DAMAGE: " + damage);
+        return damage;
+    }
 }
