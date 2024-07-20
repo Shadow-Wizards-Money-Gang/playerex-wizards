f013a4a2a2c12da367e6f423ac45dfaf26af0a72
Bibi Reden <redwitblue@gmail.com>
2024-07-20 15:54:33 -0500

    Changed keybind to minus for main screen

--- src/client/kotlin/com/bibireden/playerex/PlayerEXClient.kt
+++ src/client/kotlin/com/bibireden/playerex/PlayerEXClient.kt
@@ -22,7 +22,7 @@ import org.lwjgl.glfw.GLFW
 
 object PlayerEXClient : ClientModInitializer {
 	val MAIN_UI_SCREEN_ID = PlayerEX.id("main_ui_model")
-	val KEYBINDING_MAIN_SCREEN = KeyBindingHelper.registerKeyBinding(KeyBinding("${PlayerEX.MOD_ID}.key.main_screen", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_L, "key.categories.${PlayerEX.MOD_ID}"))
+	val KEYBINDING_MAIN_SCREEN = KeyBindingHelper.registerKeyBinding(KeyBinding("${PlayerEX.MOD_ID}.key.main_screen", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_MINUS, "key.categories.${PlayerEX.MOD_ID}"))
 
 	fun sendUpdatePacket(packet: NetworkingPackets.Update) {
 		NetworkingChannels.MODIFY.clientHandle().send(packet)
