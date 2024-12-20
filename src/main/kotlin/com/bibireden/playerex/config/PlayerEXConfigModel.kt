2a671e1444b748ecc6a232748387491ee9dc73c4
Santy <42287552+majorsanty@users.noreply.github.com>
2024-12-20 00:32:25 -0300

    Fixed mostly?

--- src/main/kotlin/com/bibireden/playerex/config/PlayerEXConfigModel.kt
+++ src/main/kotlin/com/bibireden/playerex/config/PlayerEXConfigModel.kt
@@ -10,79 +10,177 @@ import io.wispforest.owo.ui.core.Color
 @Modmenu(modId = PlayerEX.MOD_ID)
 @Config(name = "playerex-config", wrapperName = "PlayerEXConfig")
 class PlayerEXConfigModel {
+    @SectionHeader("server_options")
+
+    @JvmField @Nest @Expanded var levelingSettings = LevelingSettings()
+    @JvmField @Nest @Expanded var featureSettings = FeatureSettings()
+    @JvmField @Nest @Expanded var lifecycleSettings = LifecycleSettings()
+    @JvmField @Nest @Expanded var advancedSettings = AdvancedSettings()
+    @JvmField @Nest @Expanded var weaponLevelingSettings = WeaponXpSettings()
+    @JvmField @Nest @Expanded var armorLevelingSettings = ArmorXpSettings()
+
     @SectionHeader("client_options")
 
-    @Sync(SyncMode.NONE)
-    @JvmField
-    var tooltip: Tooltip = Tooltip.Vanilla
+    @JvmField @Nest @Expanded var visualSettings = VisualSettings()
+    @JvmField @Nest @Expanded var soundSettings = SoundSettings()
 
-    @Sync(SyncMode.NONE)
-    @JvmField
-    var showLevelOnNameplates: Boolean = true
+    // SERVER
+
+    data class ArmorXpSettings(
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var enabled: Boolean = true,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var armorPerLevel: Double = 0.1,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var reductionPerLevel: Double = 0.1,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        @RangeConstraint(min = 0.0, max = 25.0)
+        var maxReduction: Double = 25.0,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var formula: String = "5x^(1.1)",
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var xpFromPassive: Int = 10,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var xpFromHostile: Int = 20,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var xpFromMiniboss: Int = 50,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var xpFromBoss: Int = 100,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var maxLevel: Int = 500,
+    )
+
+    data class WeaponXpSettings(
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var enabled: Boolean = true,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var damagePerLevel: Double = 0.1,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var formula: String = "5x^(1.1)",
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var xpFromPassive: Int = 10,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var xpFromHostile: Int = 20,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var xpFromMiniboss: Int = 50,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var xpFromBoss: Int = 100,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var maxLevel: Int = 500,
+    )
+
+    data class LevelingSettings(
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var skillPointsPerLevelUp: Int = 1,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        @Hook
+        var levelFormula: String = "stairs(x,0.2,2.4,17,10,25)"
+    )
+
+    data class FeatureSettings(
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var resetOnDeath: Boolean = false,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var itemBreakingEnabled: Boolean = true,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var disableUI: Boolean = false
+    )
+
+    data class LifecycleSettings(
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @RestartRequired
+        @JvmField
+        var healthRegeneration: Lifecycle = Lifecycle.ON_EVERY_SECOND,
+    )
+
+    data class AdvancedSettings(
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var restorativeForceTicks: Int = 600,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var restorativeForceMultiplier: Int = 110,
+
+        @Sync(SyncMode.OVERRIDE_CLIENT)
+        @JvmField
+        var expNegationFactor: Int = 95
+    )
+
+    // CLIENT
+    data class VisualSettings(
+        @Sync(SyncMode.NONE)
+        @JvmField
+        var showLevelOnNameplates: Boolean = true,
+
+        @Sync(SyncMode.NONE)
+        @JvmField
+        var nameplateColor: Color = Color.ofRgb(0xFFAA00),
+
+        @Sync(SyncMode.NONE)
+        @JvmField
+        var tooltip: Tooltip = Tooltip.Vanilla
+    )
 
     data class SoundSettings(
         @Sync(SyncMode.NONE)
         @JvmField
-        @RangeConstraint(min = 0.0, max = 150.0)
+        @RangeConstraint(min = 0.0, max = 100.0)
         var levelUpVolume: Int = 100,
 
         @Sync(SyncMode.NONE)
         @JvmField
-        @RangeConstraint(min = 0.0, max = 150.0)
+        @RangeConstraint(min = 0.0, max = 100.0)
         var skillUpVolume: Int = 100,
 
         @Sync(SyncMode.NONE)
         @JvmField
-        @RangeConstraint(min = 0.0, max = 150.0)
+        @RangeConstraint(min = 0.0, max = 100.0)
         var refundVolume: Int = 100
     )
 
-    @JvmField @Nest var soundSettings = SoundSettings()
-
-    data class VisualSettings(
-        @Sync(SyncMode.NONE)
-        @JvmField
-        var nameplateColor: Color = Color.ofRgb(0xFFAA00),
-    )
-
-    @JvmField @Nest var visualSettings = VisualSettings()
-
-    @SectionHeader("server_options")
-    @Sync(SyncMode.OVERRIDE_CLIENT)
-    @JvmField
-    var resetOnDeath: Boolean = false
-
-    @Sync(SyncMode.OVERRIDE_CLIENT)
-    @JvmField
-    var itemBreakingEnabled: Boolean = true
-
-    @Sync(SyncMode.OVERRIDE_CLIENT)
-    @JvmField
-    var disableUI: Boolean = false
-
-    @Sync(SyncMode.OVERRIDE_CLIENT)
-    @JvmField
-    var skillPointsPerLevelUp: Int = 1
-
-    @Sync(SyncMode.OVERRIDE_CLIENT)
-    @JvmField
-    @Hook
-    var levelFormula: String = "5+(0.1*x^2)+(x+0+abs(x+0))/2"
-
-//    @JvmField
-//    var expression: Expression
-
-    @Sync(SyncMode.OVERRIDE_CLIENT)
-    @JvmField
-    var restorativeForceTicks: Int = 600
-
-    @Sync(SyncMode.OVERRIDE_CLIENT)
-    @JvmField
-    var restorativeForceMultiplier: Int = 110
-
-    @Sync(SyncMode.OVERRIDE_CLIENT)
-    @JvmField
-    var expNegationFactor: Int = 95
-
     enum class Tooltip { Default, Vanilla, PlayerEX }
+    enum class Lifecycle { ON_TICK, ON_EVERY_SECOND }
 }
--- src/main/kotlin/com/bibireden/playerex/factory/EventFactory.kt
+++ src/main/kotlin/com/bibireden/playerex/factory/EventFactory.kt
@@ -4,21 +4,33 @@ import com.bibireden.data_attributes.api.DataAttributesAPI
 import com.bibireden.playerex.PlayerEX
 import com.bibireden.playerex.api.attribute.PlayerEXAttributes
 import com.bibireden.playerex.components.player.PlayerDataComponent
+import com.bibireden.playerex.config.PlayerEXConfig
 import com.bibireden.playerex.ext.component
+import com.bibireden.playerex.predicate.FilterCheck
 import com.bibireden.playerex.registry.DamageModificationRegistry
+import com.bibireden.playerex.util.PlayerEXUtil
+import net.fabricmc.fabric.api.loot.v2.LootTableSource
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalEntityTypeTags
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.server.packs.resources.ResourceManager
 import net.minecraft.world.damagesource.DamageSource
 import net.minecraft.world.effect.MobEffects
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.MobCategory
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.entity.projectile.AbstractArrow
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.storage.loot.LootDataManager
+import net.minecraft.world.level.storage.loot.LootTable
 
 object EventFactory {
     fun reset(oldPlayer: ServerPlayer, newPlayer: ServerPlayer, isAlive: Boolean)
     {
-        val factor = if (PlayerEX.CONFIG.resetOnDeath) 0 else 100
+        val factor = if (PlayerEX.CONFIG.featureSettings.resetOnDeath) 0 else 100
 
         val takeLevels = 1 + (if (oldPlayer.hasEffect(MobEffects.LUCK)) -1 else 0) + (if (oldPlayer.hasEffect(MobEffects.BAD_OMEN)) 1 else 0)
         // attempt reconciliation
@@ -104,4 +116,75 @@ object EventFactory {
         return original
     }
 
+    fun entityWasKilled(level: Level, entity: Entity, killedEntity: Entity) {
+        when (entity) {
+            is Player -> {
+                val mainHandItem: ItemStack = entity.mainHandItem
+                tryLevelItem(mainHandItem, PlayerEX.CONFIG.weaponLevelingSettings, killedEntity)
+
+                for (item in entity.armorSlots) {
+                    tryLevelItem(item, PlayerEX.CONFIG.armorLevelingSettings, killedEntity)
+                }
+            }
+        }
+    }
+
+    private fun tryLevelItem(item: ItemStack, xpSettings: PlayerEXConfig.ArmorXpSettings, killedEntity: Entity) {
+        tryLevelItem(item,
+            xpSettings.enabled,
+            xpSettings.xpFromPassive,
+            xpSettings.xpFromBoss,
+            xpSettings.xpFromHostile,
+            xpSettings.maxLevel,
+            killedEntity
+        )
+    }
+
+    private fun tryLevelItem(item: ItemStack, xpSettings: PlayerEXConfig.WeaponXpSettings, killedEntity: Entity) {
+        tryLevelItem(item,
+            xpSettings.enabled,
+            xpSettings.xpFromPassive,
+            xpSettings.xpFromBoss,
+            xpSettings.xpFromHostile,
+            xpSettings.maxLevel,
+            killedEntity
+        )
+    }
+
+    private fun tryLevelItem(
+        item: ItemStack,
+        enabled: Boolean,
+        xpFromPassive: Int,
+        xpFromBoss: Int,
+        xpFromHostile: Int,
+        maxLevel: Int,
+        killedEntity: Entity
+    ) {
+        if (PlayerEXUtil.isLevelable(item) && enabled) {
+            PlayerEXUtil.levelItem(item, if (killedEntity.type.category.isFriendly) {
+                xpFromPassive
+            } else if (killedEntity.type.category == MobCategory.MONSTER) {
+                if (killedEntity.type.`is`(ConventionalEntityTypeTags.BOSSES)) {
+                    xpFromBoss
+                } else {
+                    xpFromHostile
+                }
+            } else {
+                0
+            }, maxLevel)
+        }
+    }
+
+    fun onModifyLootTable(
+        resourceManager: ResourceManager,
+        lootManager: LootDataManager,
+        id: ResourceLocation,
+        builder: LootTable.Builder,
+        source: LootTableSource
+    ) {
+
+        builder.apply {
+            FilterCheck()
+        }
+    }
 }
